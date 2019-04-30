66712f367fa1181151c8cf9b134b0ca773288e4e
Vanessa Markwardt <vmarkwardt@web.de>
2019-04-30 12:04:18 +0200

    Rename CardList -> CardPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Header } from './Header';
 import { Navigation } from './Navigation';
-import { CardList } from './CardList';
+import { CardPage } from './CardPage';
+import { CreatePage } from './CreatePage';
 
 export class App {
   constructor() {
     this.headerEl = new Header();
-    this.cardListEl = new CardList();
+    this.cardListEl = new CardPage();
+    this.createPage = new CreatePage();
     this.navigationEl = new Navigation();
   }
 }
---
+++ src/CardPage.js
@@ -0,0 +1,37 @@
+import { createEl } from './utils';
+import { Card } from './Card';
+
+export class CardPage {
+  constructor(target) {
+    this.cardList = [
+      { title: 'test', content: 'dies ist ein test', isBookmarked: false }
+    ];
+    this.el = createEl({
+      className: 'main__content content-card',
+      type: 'section',
+      target: target
+    });
+    this.render();
+  }
+
+  /* get cardList() {
+    return cardList;
+  }
+
+  set cardList(cards) {
+    this.cardList = [...cards];
+  } */
+
+  render() {
+    this.cardList
+      .map(
+        (card) =>
+          new Card({
+            ...card,
+            target: this.el
+          })
+      )
+      .map((card) => card.el)
+      .forEach((cardEl) => this.el.appendChild(cardEl));
+  }
+}
