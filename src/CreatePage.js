2d9aa2e7869729c191c02ec841c941d04f1264dc
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-01 15:23:42 +0200

    Create 2 htmlEl in CreatePage: main section, form

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,4 +1,24 @@
+import { createEl, getEl } from './utils';
 
 export class CreatePage {
-  
+  constructor(target, onSubmit) {
+    this.el = createEl({
+      className: 'main__content content-create hidden',
+      type: 'section',
+      target
+    });
+
+    renderForm(onSubmit);
+  }
+
+  renderForm() {
+    this.formEl = createEl({
+      type: 'form',
+      className: 'form__create-card',
+      target: this.el,
+      innerHTML: get('form--template').innerHTML
+    });
+
+    this.formEl.addEventListener('submit', onSubmit);
+  }
 }
