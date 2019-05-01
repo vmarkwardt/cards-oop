0e30f22de09a0a699a1d42ddf7948706372ef807
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-01 15:21:43 +0200

    Add header afterbegin of div#app

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,15 @@
-import { createEl } from "./utils";
+import { createEl } from './utils';
 
 export class Header {
   constructor(target) {
     this.innerHTML = '<h1 class="header-title fontweight">Home</h1>';
 
     this.el = createEl({
-      className: "header-heading",
-      type: "h1",
+      className: 'header-heading',
+      type: 'h1',
       target: target,
-      innerHTML: this.innerHTML
+      innerHTML: this.innerHTML,
+      position: 'afterbegin'
     });
   }
 }
